1f966380e8f43323e6520a68c8ec5c6e16a68478
Balasekar Natarajan <banatara@in.ibm.com>
2025-11-11 16:05:21 +0530

    Fix: Remove target directories from git cache

--- liberty-cluster-app-ear/target/liberty/wlp/lib/platform/checksums/com.ibm.websphere.appserver.binary.logging-1.0.cs
+++
@@ -1,7 +0,0 @@
-#Tue Jan 04 00:19:39 GMT 2022
-lib/com.ibm.ws.logging.hpel.osgi_1.0.60.jar=462af291e641e80370d2c1a2d3183e75
-bin/tools/ws-binarylogviewer.jar=c00af6fa70846e0dc60ff2d394f361bd
-lib/platform/binaryLogging-1.0.mf=6931e71b78d12c1e3340e8dacbac8326
-lib/com.ibm.ws.logging.hpel_1.0.60.jar=7aa635d0475eb29b0a2e0329758c7d4d
-dev/api/ibm/javadoc/com.ibm.websphere.appserver.api.hpel_2.0-javadoc.zip=d8b5de8de0706860bd307084be2c735d
-dev/api/ibm/com.ibm.websphere.appserver.api.hpel_2.0.60.jar=34d7bd6bc9b14568721ab52ec961623d
